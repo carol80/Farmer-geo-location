27765654d02e4c5423de9e664595440a9bbfd28c
Princeton <princebaretto@gmail.com>
2020-01-25 15:48:55 +0530

    all charts

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var logger = require('morgan')
 var bodyParser = require('body-parser')
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const session = require('express-session');
+var XLSX = require("xlsx");
+const fetch = require('node-fetch');
 const http = require('http');
 
 var admin = require('firebase-admin')
@@ -94,6 +96,37 @@ app.post("/saveCoordinate", function(req, res) {
     .ref("pending/" + whatsapp)
     .update({ coordinates: req.body.coordinate, area: req.body.area });
   console.log(req.body);
+});
+
+
+app.get("/getExcelData", (req, res) => {
+  var workbook = XLSX.readFile(__dirname + "/views/excel/crop_data.xlsx");
+  var sheet_name_list = workbook.SheetNames;
+  var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+  // console.log(xlData[0]);
+  var rainfall_list = [];
+  var crop_list = [];
+  var humid_list = [];
+  var irri_list = [];
+  var min_temp_list = [];
+  var max_temp_list = [];
+  xlData.forEach(row => {
+    rainfall_list.push(row.Rainfall);
+    crop_list.push(row.Crops);
+    humid_list.push(row.Humidity);
+    irri_list.push(row.Irrigation);
+    min_temp_list.push(row.Min);
+    max_temp_list.push(row.Max);
+  });
+  my_json = {
+    rainfall: rainfall_list,
+    crops: crop_list,
+    humidity: humid_list,
+    irri: irri_list,
+    min: min_temp_list,
+    max: max_temp_list
+  };
+  res.json(my_json);
 });
 
 // app.post("/farm_result", function(req, res) {
